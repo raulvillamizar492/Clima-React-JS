2a4cd9fbcc543eb524c86575879b06af651f2ad3
Raul Andres Villamizar <69433414+raulvillamizar492@users.noreply.github.com>
2020-11-04 21:23:17 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from './Components/Header';
+import Formulario from './Components/Formulario';
+import Clima from './Components/Clima';
+import Error from './Components/Error';
+
+function App() {
+
+    // state del formulario
+  const [busqueda, guardarBusqueda] = useState({ 
+      ciudad: '',
+      pais: ''
+  });
+  const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+  const [error, guardarError] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+
+        if(consultar) {
+          const appId = 'cd1a6b84e38ce3f21d237cef7d500bca';
+          const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+  
+          const respuesta = await fetch(url);
+          const resultado = await respuesta.json();
+  
+          guardarResultado(resultado);
+          guardarConsultar(false);
+
+          // Detecta si hubo resultados correctos en la consulta
+
+          if(resultado.cod === "404") {
+              guardarError(true);
+          } else {
+              guardarError(false);
+          }
+        }
+        
+    }
+    consultarAPI();
+    // eslint-disable-next-line
+  },[consultar]);
+
+  let componente;
+  if(error) {
+    componente = <Error mensaje="No hay resultados" />
+  } else {
+    componente = <Clima 
+                    resultado={resultado}
+                />
+  }
+
+
+
+  return (
+    <Fragment>
+        <Header 
+          titulo='Sistema De Clima React JS'
+        />
+
+        <div className="contenedor-form">
+            <div className="container">
+                <div className="row">
+                    <div className="col m6 s12">
+                        <Formulario 
+                          busqueda={busqueda}
+                          guardarBusqueda={guardarBusqueda}
+                          guardarConsultar={guardarConsultar}
+                        />
+                    </div>
+                    <div className="col m6 s12">
+                        {componente}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Fragment>
+  );
+}
+
+export default App;
